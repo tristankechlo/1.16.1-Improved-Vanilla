57d6c54872fbc1bf9b1b362235e36e5dd61b3a24
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2020-07-19 01:03:15 +0200

    Update MobDropHandler.java

--- src/main/java/com/tristankechlo/improvedvanilla/eventhandler/MobDropHandler.java
+++ src/main/java/com/tristankechlo/improvedvanilla/eventhandler/MobDropHandler.java
@@ -2,34 +2,42 @@ package com.tristankechlo.improvedvanilla.eventhandler;
 
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 
 import com.tristankechlo.improvedvanilla.config.ImprovedVanillaConfig;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraft.util.IItemProvider;
-import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 
 public class MobDropHandler {
-	
+	    
     @SubscribeEvent
-    public void onMobDrop(final LivingDropsEvent event) {
-        final Entity entity = event.getEntity();
-        final EntityType<?> type = (EntityType<?>)entity.getType();
-        
-        final int dropchance = ImprovedVanillaConfig.SERVER.mobSpawnEggDropChance.get();
-        if(dropchance >= 1 && dropchance <= 100) {
-            if (Math.random() < ((double)dropchance / 100)) {
-                final ItemStack stack = new ItemStack((IItemProvider)ForgeRegistries.ITEMS.getValue(new ResourceLocation(type.getRegistryName() + "_spawn_egg")));
-                final ItemEntity itemEntity = new ItemEntity(entity.getEntityWorld(), entity.getPosX(), entity.getPosY(), entity.getPosZ(), stack);
-                event.getDrops().add(itemEntity);
-            }
-        }
-       
-        
+    public void onMobDeath(final LivingDeathEvent event) {
+    	final LivingEntity entity = event.getEntityLiving();
+    	final World world = entity.world;
+    	if(!world.isRemote){
+        	final Entity source = event.getSource().getTrueSource();
+        	if(source instanceof ServerPlayerEntity) {
+        		//final ServerPlayerEntity player = (ServerPlayerEntity)entity;
+
+                final int dropchance = ImprovedVanillaConfig.SERVER.mobSpawnEggDropChance.get();
+                if(dropchance >= 1 && dropchance <= 100) {
+                    if (Math.random() < ((double)dropchance / 100)) {
+                    	
+                        final EntityType<?> type = (EntityType<?>)entity.getType();
+                        final ItemStack stack = new ItemStack((IItemProvider)ForgeRegistries.ITEMS.getValue(new ResourceLocation(type.getRegistryName() + "_spawn_egg")), 1);
+                		entity.entityDropItem(stack);
+                		
+                    }
+                }
+        	}   		
+    	}
     }
     
 }
