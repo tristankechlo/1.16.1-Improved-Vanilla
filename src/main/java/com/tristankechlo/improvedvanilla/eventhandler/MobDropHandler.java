a89c492c8a8ceeec697bdbc0b0d3e8e72de6c873
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2020-07-19 13:12:58 +0200

    Update MobDropHandler.java

--- src/main/java/com/tristankechlo/improvedvanilla/eventhandler/MobDropHandler.java
+++ src/main/java/com/tristankechlo/improvedvanilla/eventhandler/MobDropHandler.java
@@ -18,40 +18,39 @@ import net.minecraft.util.IItemProvider;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 
 public class MobDropHandler {
-	    
-    @SubscribeEvent
-    public void onMobDeath(final LivingDeathEvent event) {
-    	final LivingEntity entity = event.getEntityLiving();
-    	final World world = entity.world;
-    	if(!world.isRemote){
-    		
-        	final Entity source = event.getSource().getTrueSource();
-        	if(source instanceof ServerPlayerEntity) {
-        		
-        		final ServerPlayerEntity player = (ServerPlayerEntity)source;
-        		if(player.isSpectator()) {
-        			return;
-        		}
 
-        		final int lootingLevel = EnchantmentHelper.getEnchantmentLevel(Enchantments.LOOTING, player.getHeldItemMainhand());
-                final int dropchance = ImprovedVanillaConfig.SERVER.mobSpawnEggDropChance.get();
-                
-                if(dropchance >= 1 && dropchance <= 100) {
+	@SubscribeEvent
+	public void onMobDeath(final LivingDeathEvent event) {
+		final LivingEntity entity = event.getEntityLiving();
+		final World world = entity.world;
+		if (!world.isRemote) {
 
-                	//foreach lootinglevel there's an additional chance to drop the egg
-	        		for (int i = 0; i < (1 + lootingLevel); i++) {
-	        			
-	                    if (Math.random() < ((double)dropchance / 100)) {
-	                    	
-	                        final EntityType<?> type = (EntityType<?>)entity.getType();
-	                        final ItemStack stack = new ItemStack((IItemProvider)ForgeRegistries.ITEMS.getValue(new ResourceLocation(type.getRegistryName() + "_spawn_egg")), 1);
-	                		entity.entityDropItem(stack);
-	                		
-	                    }
+			final Entity source = event.getSource().getTrueSource();
+			if (source instanceof ServerPlayerEntity) {
+
+				final ServerPlayerEntity player = (ServerPlayerEntity) source;
+				if (player.isSpectator()) {
+					return;
+				}
+
+				final int lootingLevel = EnchantmentHelper.getEnchantmentLevel(Enchantments.LOOTING, player.getHeldItemMainhand());
+				final int dropchance = ImprovedVanillaConfig.SERVER.mobSpawnEggDropChance.get();
+
+				if (dropchance >= 1 && dropchance <= 100) {
+
+					// foreach lootinglevel there's an additional chance to drop the egg
+					for (int i = 0; i < (1 + lootingLevel); i++) {
+
+						if (Math.random() < ((double) dropchance / 100)) {
+
+							final EntityType<?> type = (EntityType<?>) entity.getType();
+							final ItemStack stack = new ItemStack((IItemProvider) ForgeRegistries.ITEMS.getValue(new ResourceLocation(type.getRegistryName() + "_spawn_egg")), 1);
+							entity.entityDropItem(stack);
+
+						}
 					}
-                }
-        	}   		
-    	}
-    }
-    
+				}
+			}
+		}
+	}
 }
