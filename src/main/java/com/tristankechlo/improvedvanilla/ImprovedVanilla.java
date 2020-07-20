b8a42263da315f0a1313bfd050f749cd6b3b3cc0
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2020-07-20 15:21:55 +0200

    Update ImprovedVanilla.java

--- src/main/java/com/tristankechlo/improvedvanilla/ImprovedVanilla.java
+++ src/main/java/com/tristankechlo/improvedvanilla/ImprovedVanilla.java
@@ -1,10 +1,8 @@
 package com.tristankechlo.improvedvanilla;
 
 import org.apache.logging.log4j.LogManager;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+import net.minecraftforge.eventbus.api.IEventBus;
 import com.tristankechlo.improvedvanilla.eventhandler.MobDropHandler;
 import com.tristankechlo.improvedvanilla.config.ImprovedVanillaConfig;
 import com.tristankechlo.improvedvanilla.eventhandler.CropRightClickHandler;
@@ -24,28 +22,22 @@ public class ImprovedVanilla {
     public static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "improvedvanilla";
     
-    public ImprovedVanilla() {
-        ImprovedVanilla.instance = this;
+    @SuppressWarnings("unused")
+	public ImprovedVanilla() {
+		final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, ImprovedVanillaConfig.spec);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
         
         MinecraftForge.EVENT_BUS.register(new SpawnerHandler());
         MinecraftForge.EVENT_BUS.register(new CropRightClickHandler());
         MinecraftForge.EVENT_BUS.register(new MobDropHandler());
         
+        
+        //ModEntities.ENTITIES.register(modEventBus);
+        
+
+        ImprovedVanilla.instance = this;
         MinecraftForge.EVENT_BUS.register((Object)this);
     }
-    
-    private void setup(final FMLCommonSetupEvent event) {
-    }
-    
-    private void doClientStuff(final FMLClientSetupEvent event) {
-    }
-    
-    @SubscribeEvent
-    public void onServerStarting(final FMLServerStartingEvent event) {
-    }
-    
+            
 }
