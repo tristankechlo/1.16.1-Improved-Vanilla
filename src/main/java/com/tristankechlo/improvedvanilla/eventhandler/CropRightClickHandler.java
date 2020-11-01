fa46aa2a17840fdac87afda850c4831ef0ad53a6
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2020-11-01 16:10:46 +0100

    bugfixes/improvements

--- src/main/java/com/tristankechlo/improvedvanilla/eventhandler/CropRightClickHandler.java
+++ src/main/java/com/tristankechlo/improvedvanilla/eventhandler/CropRightClickHandler.java
@@ -1,33 +1,31 @@
 package com.tristankechlo.improvedvanilla.eventhandler;
 
-import net.minecraft.item.ItemStack;
+import java.util.Collections;
 import java.util.List;
 
 import com.tristankechlo.improvedvanilla.config.ImprovedVanillaConfig;
 
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.world.server.ServerWorld;
-import net.minecraft.block.BlockState;
-import java.util.Collection;
-import java.util.Collections;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraft.state.IntegerProperty;
 import net.minecraft.block.Block;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.world.World;
-import net.minecraft.util.Hand;
-import net.minecraft.state.Property;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.CocoaBlock;
-import net.minecraft.block.BeetrootBlock;
 import net.minecraft.block.CropsBlock;
-import net.minecraft.item.Items;
+import net.minecraft.block.NetherWartBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.state.IntegerProperty;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
 public class CropRightClickHandler {
-	
+
     @SubscribeEvent
     public void onPlayerRightClickBlock(final PlayerInteractEvent.RightClickBlock event) {
         final World world = event.getWorld();
@@ -36,51 +34,46 @@ public class CropRightClickHandler {
         if (player == null || world == null) {
             return;
         }
-        if (world.isRemote || player.isCreative() || player.isSneaking() || player.isSpectator() || event.getHand() != Hand.MAIN_HAND) {
+        if (world.isRemote || player.isSneaking() || player.isSpectator() || event.getHand() != Hand.MAIN_HAND) {
             return;
         }
         if(ImprovedVanillaConfig.SERVER.enableRightClickCrops.get() == false) {
         	return;
         }
-        if (player.getHeldItemMainhand().getItem() != Items.AIR) {
+        if (!player.getHeldItemMainhand().isEmpty()) {
             return;
         }
         final Block targetBlock = world.getBlockState(pos).getBlock();
-        Block block;
         IntegerProperty age;
         if (targetBlock instanceof CropsBlock) {
-            block = targetBlock;
-            if (targetBlock instanceof BeetrootBlock) {
-                age = BeetrootBlock.BEETROOT_AGE;
-            } else {
-                age = CropsBlock.AGE;
-            }
-        } else {
-            if (!(targetBlock instanceof CocoaBlock)) {
-                return;
-            }
-            block = targetBlock;
+        	age = ((CropsBlock)targetBlock).getAgeProperty();
+        } else if (targetBlock.equals(Blocks.COCOA)) {
             age = CocoaBlock.AGE;
+        } else if (targetBlock.equals(Blocks.NETHER_WART)) {
+            age = NetherWartBlock.AGE;
+        } else {
+            return;
         }
-        if (this.spawnDropsAndResetBlock(world, pos, block, (Property<Integer>)age)) {
+        
+        if (this.spawnDropsAndResetBlock(world, pos, age)) {
             player.swingArm(Hand.MAIN_HAND);
             event.setCanceled(true);
         }
     }
     
-    private boolean spawnDropsAndResetBlock(final World world, final BlockPos pos, final Block block, final Property<Integer> property) {
+    private boolean spawnDropsAndResetBlock(final World world, final BlockPos pos, final IntegerProperty property) {
         final BlockState blockState = world.getBlockState(pos);
         if (blockState == null) {
             return false;
         }
-        final int maximumAge = Collections.max((Collection<? extends Integer>)property.getAllowedValues());
-        final int currentAge = (int)blockState.get(property);
+        final int maximumAge = Collections.max(property.getAllowedValues());
+        final int currentAge = blockState.get(property);
         if (currentAge < maximumAge) {
             return false;
         }
-        final BlockState newState = (BlockState)blockState.with(property, 0);
+        final BlockState newState = blockState.with(property, 0);
         world.setBlockState(pos, newState, 3);
-        final List<ItemStack> drops = (List<ItemStack>)Block.getDrops(blockState, (ServerWorld)world, pos, (TileEntity)null);
+        final List<ItemStack> drops = Block.getDrops(blockState, (ServerWorld)world, pos, (TileEntity)null);
         drops.forEach(stack -> Block.spawnAsEntity(world, pos, stack));
         return true;
     }
