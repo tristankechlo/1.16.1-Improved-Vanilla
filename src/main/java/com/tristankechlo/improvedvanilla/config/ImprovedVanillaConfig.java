5bd7124cde59fec09fdcdbc6b4df8c065f422321
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2021-03-05 17:25:27 +0100

    small improvements/port to 1.16.5

--- src/main/java/com/tristankechlo/improvedvanilla/config/ImprovedVanillaConfig.java
+++ src/main/java/com/tristankechlo/improvedvanilla/config/ImprovedVanillaConfig.java
@@ -19,7 +19,7 @@ public class ImprovedVanillaConfig {
 
 	public static class Server {
 
-		public final BooleanValue enableRightClickCrops;		
+		public final BooleanValue enableRightClickCrops;
 		public final BooleanValue enableEasyPlanting;
 		public final ConfigValue<Integer> easyPlantingRadius;
 		public final BooleanValue easyPlantingCircle;
@@ -33,32 +33,37 @@ public class ImprovedVanillaConfig {
 
 		Server(ForgeConfigSpec.Builder builder) {
 			builder.comment("farming related configs").push("Farming");
-			enableRightClickCrops = builder.comment("If set to true, Crops can be collected by rightclicking them").define("enableRightClickCrops", true);
-			enableEasyPlanting = builder.comment("If set to true, seeds will be planted in a radius when right clicked on a farm land").define("enableEasyPlanting", true);
-			easyPlantingRadius = builder.comment("the radius in which the seeds will be placed").define("easyPlantingRadius", 4);			
-			easyPlantingCircle = builder.comment("if set to true, the seeds will be planted in acircle instead of a square").define("easyPlantingCircle", true);
+			enableRightClickCrops = builder.comment("If set to true, Crops can be collected by rightclicking them")
+					.define("enableRightClickCrops", true);
+			enableEasyPlanting = builder
+					.comment("If set to true, seeds will be planted in a radius when right clicked on a farm land")
+					.define("enableEasyPlanting", true);
+			easyPlantingRadius = builder.comment("the radius in which the seeds will be placed")
+					.define("easyPlantingRadius", 4);
+			easyPlantingCircle = builder
+					.comment("if set to true, the seeds will be planted in acircle instead of a square")
+					.define("easyPlantingCircle", true);
 			builder.pop();
-			
 
-			builder.comment("Spawner related settings (not affective when mod \"SpawnerSettings\" is present)").push("Spawner");
-			spawnerDropChance = builder
-					.comment("Drop-chance for the spawner to drop itself when mined with a silk-touch pickaxe (default 100, 100 -> always, 0 -> never)")
+			builder.comment("Spawner related settings (not affective when mod \"SpawnerSettings\" is present)")
+					.push("Spawner");
+			spawnerDropChance = builder.comment(
+					"Drop-chance for the spawner to drop itself when mined with a silk-touch pickaxe (default 100, 100 -> always, 0 -> never)")
 					.defineInRange("spawnerDropChance", 100, 0, 100);
 			spawnEggDropChanceOnSpawnerDestroyed = builder
 					.comment("Drop-chance for each stack, in a spawner, in % (default 100, 100 -> always, 0 -> never)")
 					.defineInRange("spawnEggDropChanceOnSpawnerDestroyed", 100, 0, 100);
 			builder.pop();
 
-			
 			builder.comment("").push("Mob-Drops");
 			dropOnlyWhenKilledByPlayer = builder
 					.comment("If set to true, SpawnEggs only drop when the mob was killed by a player")
 					.define("dropOnlyWhenKilledByPlayer", true);
-			lootingAffective = builder
-					.comment("If set to true, then foreach looting level on the players tool, there will by another possibility to drop the egg")
+			lootingAffective = builder.comment(
+					"If set to true, then foreach looting level on the players tool, there will by another possibility to drop the egg")
 					.define("lootingAffective", true);
-			mobSpawnEggDropChance = builder
-					.comment("Drop-chance for all mobs to drop their spawn-egg in % (default 2, 100 -> always, 0 -> never)")
+			mobSpawnEggDropChance = builder.comment(
+					"Drop-chance for all mobs to drop their spawn-egg in % (default 2, 100 -> always, 0 -> never)")
 					.defineInRange("mobSpawnEggDropChance", 2, 0, 100);
 			builder.pop();
 		}
--- src/main/java/com/tristankechlo/improvedvanilla/eventhandler/MobDropHandler.java
+++ src/main/java/com/tristankechlo/improvedvanilla/eventhandler/MobDropHandler.java
@@ -1,120 +1,96 @@
 package com.tristankechlo.improvedvanilla.eventhandler;
 
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import com.tristankechlo.improvedvanilla.config.ImprovedVanillaConfig;
+
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.item.ItemEntity;
-
-import com.tristankechlo.improvedvanilla.config.ImprovedVanillaConfig;
-
-import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.enchantment.Enchantments;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.event.entity.living.LivingDeathEvent;
 
 public class MobDropHandler {
 
 	@SubscribeEvent
-	public void onMobDeath(final LivingDeathEvent event) {
+	public void onMobDeath(final LivingDropsEvent event) {
 		final LivingEntity entity = event.getEntityLiving();
 		final World world = entity.world;
-		if (!world.isRemote) {
+		if (world.isRemote) {
+			return;
+		}
 
-			final boolean onlyWhenKilledByPlayer = ImprovedVanillaConfig.SERVER.dropOnlyWhenKilledByPlayer.get();
-			final int dropchance = ImprovedVanillaConfig.SERVER.mobSpawnEggDropChance.get();
-			final Entity source = event.getSource().getTrueSource();
-			final EntityType<?> type = (EntityType<?>) entity.getType();
-			final Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(type.getRegistryName() + "_spawn_egg"));
-			
-			//killed by player and onlyWhenKilledByPlayer is true
-			if ((source instanceof ServerPlayerEntity) && onlyWhenKilledByPlayer) {
-				//drop only when entity was killed by a player
+		final boolean onlyWhenKilledByPlayer = ImprovedVanillaConfig.SERVER.dropOnlyWhenKilledByPlayer.get();
+		final int dropchance = ImprovedVanillaConfig.SERVER.mobSpawnEggDropChance.get();
+		final Entity source = event.getSource().getTrueSource();
+		final BlockPos pos = entity.getPosition();
+		final EntityType<?> type = (EntityType<?>) entity.getType();
+		final Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(type.getRegistryName() + "_spawn_egg"));
+
+		// killed by player and onlyWhenKilledByPlayer is true
+		if ((source instanceof ServerPlayerEntity) && onlyWhenKilledByPlayer) {
+			// drop only when entity was killed by a player
+			final ServerPlayerEntity player = (ServerPlayerEntity) source;
+			if (player.isSpectator()) {
+				return;
+			}
+			handleKilledByPlayer(event, world, pos, item, dropchance);
+
+		// if allowed to drop always (onlyWhenKilledByPlayer == true)
+		} else if (!onlyWhenKilledByPlayer) {
+
+			// if the mob was killed by player anyway
+			if (source instanceof ServerPlayerEntity) {
+
 				final ServerPlayerEntity player = (ServerPlayerEntity) source;
 				if (player.isSpectator()) {
 					return;
 				}
+				handleKilledByPlayer(event, world, pos, item, dropchance);
 
-				final int lootingLevel = EnchantmentHelper.getEnchantmentLevel(Enchantments.LOOTING, player.getHeldItemMainhand());
-				final boolean lootingAffective = ImprovedVanillaConfig.SERVER.lootingAffective.get();
-
+			// not killed by player
+			} else {
 				if (dropchance >= 1 && dropchance <= 100) {
-					int count = 0;
-					if(lootingAffective) {
-						// foreach lootinglevel there's an additional chance to drop the egg
-						for (int i = 0; i < (1 + lootingLevel); i++) {
-							if (Math.random() < ((double) dropchance / 100)) {
-								count++;
-							}
-						}
-					} else {						
-						if (Math.random() < ((double) dropchance / 100)) {
-							count++;
-						}
-					}
-					if(count > 0) {
-						final ItemStack stack = new ItemStack(item, count);
+					if (Math.random() < ((double) dropchance / 100)) {
+						final ItemStack stack = new ItemStack(item, 1);
 						ItemEntity itemEntity = new ItemEntity(world, entity.getPosX(), entity.getPosY(), entity.getPosZ(), stack);
 						itemEntity.setDefaultPickupDelay();
 						world.addEntity(itemEntity);
 					}
 				}
-				
-			//if onlyWhenKilledByPlayer is false
-			} else if (!onlyWhenKilledByPlayer) {
-				
-				//if the mob was killed by player anyway
-				if (source instanceof ServerPlayerEntity) {
-					
-					final ServerPlayerEntity player = (ServerPlayerEntity) source;
-					if (player.isSpectator()) {
-						return;
-					}
-
-					final int lootingLevel = EnchantmentHelper.getEnchantmentLevel(Enchantments.LOOTING, player.getHeldItemMainhand());
-					final boolean lootingAffective = ImprovedVanillaConfig.SERVER.lootingAffective.get();
-					
-					if (dropchance >= 1 && dropchance <= 100) {
-						int count = 0;
-						if(lootingAffective) {
-							// foreach lootinglevel there's an additional chance to drop the egg
-							for (int i = 0; i < (1 + lootingLevel); i++) {
-								if (Math.random() < ((double) dropchance / 100)) {
-									count++;
-								}
-							}
-						} else {						
-							if (Math.random() < ((double) dropchance / 100)) {
-								count++;
-							}
-						}
-						if(count > 0) {
-							final ItemStack stack = new ItemStack(item, count);
-							ItemEntity itemEntity = new ItemEntity(world, entity.getPosX(), entity.getPosY(), entity.getPosZ(), stack);
-							itemEntity.setDefaultPickupDelay();
-							world.addEntity(itemEntity);
-						}
-					}
-
-				//not killed by player
-				} else {
-					if (dropchance >= 1 && dropchance <= 100) {
-						if (Math.random() < ((double) dropchance / 100)) {
-							final ItemStack stack = new ItemStack(item, 1);
-							ItemEntity itemEntity = new ItemEntity(world, entity.getPosX(), entity.getPosY(), entity.getPosZ(), stack);
-							itemEntity.setDefaultPickupDelay();
-							world.addEntity(itemEntity);
-						}
-					}
-				}
-			} else {
-				return;
 			}
 		}
 	}
+
+	private static void handleKilledByPlayer(LivingDropsEvent event, World world, BlockPos pos, Item item, int dropchance) {
+		final int lootingLevel = event.getLootingLevel();
+		final boolean lootingAffective = ImprovedVanillaConfig.SERVER.lootingAffective.get();
+
+		int count = 0;
+		if (lootingAffective && lootingLevel >= 1) {
+			// foreach lootinglevel there's an additional chance to drop the egg
+			for (int i = 0; i < (1 + lootingLevel); i++) {
+				if (Math.random() < ((double) dropchance / 100)) {
+					count++;
+				}
+			}
+		} else {
+			if (Math.random() < ((double) dropchance / 100)) {
+				count++;
+			}
+		}
+		if (count > 0) {
+			final ItemStack stack = new ItemStack(item, count);
+			ItemEntity itemEntity = new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), stack);
+			itemEntity.setDefaultPickupDelay();
+			world.addEntity(itemEntity);
+		}
+	}
+
 }
