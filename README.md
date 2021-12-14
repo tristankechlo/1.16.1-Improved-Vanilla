6aa74fbf91ca8e8a1e0b941fb2b2b559150e6762
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2021-12-14 17:00:18 +0100

    remove structures

--- README.md
+++ README.md
@@ -13,10 +13,4 @@
  ![EasyPlaynting](https://i.ibb.co/LCLZJZK/Easy-Planting.gif)
 
 
-### added structures in version 1.17.1-1.5.0  
- - [Forgotten Well](https://github.com/tristankechlo/ImprovedVanilla/wiki/Structures#forgotten-well)
- - [Jungle Temple](https://github.com/tristankechlo/ImprovedVanilla/wiki/Structures#jungle-temple)
- - [Underground Temple](https://github.com/tristankechlo/ImprovedVanilla/wiki/Structures#underground-temple)
-
-
 [Download the mod here](https://www.curseforge.com/minecraft/mc-mods/improved-vanilla)
--- src/main/java/com/tristankechlo/improvedvanilla/ImprovedVanilla.java
+++ src/main/java/com/tristankechlo/improvedvanilla/ImprovedVanilla.java
@@ -4,22 +4,15 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.tristankechlo.improvedvanilla.config.ImprovedVanillaConfig;
-import com.tristankechlo.improvedvanilla.eventhandler.BiomeLoadingHandler;
 import com.tristankechlo.improvedvanilla.eventhandler.CropRightClickHandler;
 import com.tristankechlo.improvedvanilla.eventhandler.EasyPlantingHandler;
 import com.tristankechlo.improvedvanilla.eventhandler.MobDropHandler;
 import com.tristankechlo.improvedvanilla.eventhandler.SpawnerHandler;
-import com.tristankechlo.improvedvanilla.eventhandler.WorldLoadingHandler;
-import com.tristankechlo.improvedvanilla.init.ConfiguredStructures;
-import com.tristankechlo.improvedvanilla.init.ModStructures;
 
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ImprovedVanilla.MOD_ID)
 public class ImprovedVanilla {
@@ -30,26 +23,12 @@ public class ImprovedVanilla {
 	public ImprovedVanilla() {
 		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ImprovedVanillaConfig.spec);
 
-		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-		modEventBus.addListener(this::commonSetup);
-		ModStructures.STRUCTURES.register(modEventBus);
-
 		MinecraftForge.EVENT_BUS.register(new CropRightClickHandler());
 		MinecraftForge.EVENT_BUS.register(new EasyPlantingHandler());
 		MinecraftForge.EVENT_BUS.register(new MobDropHandler());
 		MinecraftForge.EVENT_BUS.register(new SpawnerHandler());
 
-		MinecraftForge.EVENT_BUS.register(new BiomeLoadingHandler());
-		MinecraftForge.EVENT_BUS.register(new WorldLoadingHandler());
-
 		MinecraftForge.EVENT_BUS.register(this);
-	}
-
-	private void commonSetup(final FMLCommonSetupEvent event) {
-		event.enqueueWork(() -> {
-			ModStructures.setupStructures();
-			ConfiguredStructures.registerConfiguredStructures();
-		});
 	}
 
 }
